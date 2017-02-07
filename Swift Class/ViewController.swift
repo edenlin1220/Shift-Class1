372771e287b74e75822bd94d2e8aa9521844b664
EdenLin <eden@EdenLin-MacbookPro.local>
2017-02-07 19:42:19 +0800

    Initial Commit

---
+++ Swift Class/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Class
+//
+//  Created by EdenLin on 2017/2/7.
+//  Copyright © 2017年 EdenLin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
